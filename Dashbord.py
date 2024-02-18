1ab5e78ab47443642a6c5cae2f57850853c6f5df
Miguel-Ortega <160426948+Miguel-Ortega@users.noreply.github.com>
2024-02-18 19:14:33 -0300

    Add files via upload

---
+++ Dashbord.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import requests 
+import pandas as pd
+import plotly.express as px
+from PIL import Image
+
+st.set_page_config(layout= 'wide',page_icon= 'jabuti-05.png', page_title= 'Facilitador')
+
+st.title('Tratamentos de Dados')
+
+Jabuti = Image.open('jabuti-05.png')
+Minerva = Image.open('MInerva.png')
+
+coluna1,coluna2,coluna3 = st.columns([250,250,250])
+
+
+with coluna1:
+    st.image(Jabuti,width = 200) 
+
+with coluna2:
+    st.write('Olá!!, Seja muito bem vindo!')
+    st.write('Ao lado esquerdo existe uma barra de seleção onde vc encontrara todos os softwares que te ajudam a econimars um tempinho do seu dia!')
+    st.write('Este aplicativo foi feito por Miguel Ortega em seu tempo nem tão livre.')
+
+with coluna3:
+    st.image(Minerva,width= 200)
